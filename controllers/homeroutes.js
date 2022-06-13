0882fa091e3c5651b433e54a5b3266b1d0a50799
akaur091 <amniderkaur.09@gmail.com>
2022-06-12 18:45:21 -0700

    add function that finds all posts

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -3,11 +3,42 @@ const router = require('express').Router();
 // require sequelize
 const sequelize = require('../config/connection');
 // call models
-const {Post, User, Comment} = require('../models');
+const { Post, User, Comment } = require('../models');
 // auth
 const withAuth = require('../utils/auth');
 
 // route to get
-router.get('/', withAuth, (req,res)=> {
-    
+router.get('/', withAuth, (req, res) => {
+    // function to find all posts
+    Post.findAll({
+        where: {
+            user_id: req.session.user_id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+            },
+            {
+             model: User,
+             attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({plain: true}));
+        res.render('dashboard', {posts, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
 })
