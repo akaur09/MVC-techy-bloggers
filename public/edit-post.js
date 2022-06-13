3d1bc85d4abf05cae7a66909563591b60fa7a70e
akaur091 <amniderkaur.09@gmail.com>
2022-06-13 11:15:08 -0700

    add fetch route

--- public/edit-post.js
+++ public/edit-post.js
@@ -9,4 +9,17 @@ async function editHandler(event) {
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
+
+    // route
+    const response = await fetch(`/api/posts/${id}`,{
+        method: 'PUT',
+        body: JSON.stringify({
+            post_id: id,
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
