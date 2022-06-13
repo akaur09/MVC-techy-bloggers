5b040a969cfbd297b997a983e885be7bbdf5c734
akaur091 <amniderkaur.09@gmail.com>
2022-06-13 10:53:43 -0700

    add if else statment to display the post

--- public/comment.js
+++ public/comment.js
@@ -6,5 +6,24 @@ async function commentHandler(event){
     // id is set to -1 due to array starting position being 0
     const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
-    
+    if (comment_text){
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                comment_text,
+                post_id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+            document.querySelector('#comment-form').style.display = "block";
+        }
+    }
 }
+
+document.querySelector('.comment-form').addEventListener('submit', commentHandler);
