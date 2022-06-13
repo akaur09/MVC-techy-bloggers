02b4334b119b8aa6cf11eb6ea2853daa85f5b068
akaur091 <amniderkaur.09@gmail.com>
2022-06-12 20:22:20 -0700

    add login form function

---
+++ public/login.js
@@ -0,0 +1,24 @@
+// create a function to handle login
+async function loginFormHandler(event){
+    event.prventDefault();
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    // cofirm username and password match
+    if (username && password){
+        const response = await fetch('/appi/users/login', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if (response.ok){
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
