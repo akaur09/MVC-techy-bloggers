50841a286074ddcd1cc262036809e86263497aa9
akaur091 <amniderkaur.09@gmail.com>
2022-06-13 10:31:59 -0700

    add route function

--- public/add-post.js
+++ public/add-post.js
@@ -3,5 +3,18 @@ async function addpostHandler(event) {
     event.preventDefault();
     // call on content and title
     const content = document.querySelector('textarea[name= "content"]').value;
-    const title = document.querySelector('input[name = "post-title"]')
+    const title = document.querySelector('input[name = "post-title"]').value;
+
+    // create a route for posts
+    const response = await fetch('/api/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            content
+        }),
+        headers: {
+            'Content-Type': 'applicaiton/json'
+        }
+    });
+    
 }
