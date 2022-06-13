92057231b3dda900c0748156c4ea5199def41b0f
akaur091 <amniderkaur.09@gmail.com>
2022-06-12 19:19:58 -0700

    create route to display all posts

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -3,7 +3,32 @@ const sequelize = require('../config/connection');
 // import router
 const router = require('express').Router();
 // import models
-const {Post, User, Comment} = require ('../models');
+const { Post, User, Comment } = require('../models');
 
 // find all posts on homepage
-router.get('/',)
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'content', 'created_at'],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'title', 'content', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    }).then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true}));
+        console.log(posts);
+        res.render('homepage', {posts, loggedIn: req.session.loggedIn});
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
