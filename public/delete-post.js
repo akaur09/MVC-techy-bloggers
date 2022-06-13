decbab8102802ed51a8edb28d90389abb879a984
akaur091 <amniderkaur.09@gmail.com>
2022-06-13 10:59:01 -0700

    add route to delete post

--- public/delete-post.js
+++ public/delete-post.js
@@ -6,4 +6,15 @@ async function deleteHandler(event){
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length -1
     ];
+
+    // create route that will delete the post
+    const response = await fetch(`/api/posts/${id}`, {
+        method: 'DELETE',
+        body: JSON.stringify({
+            post_id: id
+        }),
+        header: {
+            'Content-Type': 'application/json'
+        }
+    });
 }
